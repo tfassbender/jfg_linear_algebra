14dd6758b38868cf3e18ad67394c54c60496d3e4
Tobias <tobias-fassbender@gmx.de>
2017-12-12 21:04:55 +0100

    fixed bugs in Line2D class (too many...); tested Line2D class (working)

--- src/net/jfabricationgames/linear_algebra/Line2D.java
+++ src/net/jfabricationgames/linear_algebra/Line2D.java
@@ -21,22 +21,40 @@ public class Line2D {
 	 * 		If there is no crossing point because of parallel lines or there are infinite crossing points because the lines are equal all coordinates are Double.NaN.
 	 */
 	public Vector2D calculateCrosspoint(Line2D line) {
-		double a1 = direction.y-start.y;
+		/*double a1 = direction.y-start.y;
 		double b1 = start.x-direction.x;
-		double c1 = start.x*start.y - start.x*direction.y;//TODO check here
+		double c1 = a1 * start.x - b1 * start.y;
 		
 		double a2 = line.direction.y-line.start.y;
 		double b2 = line.start.x-line.direction.x;
-		double c2 = line.direction.x*line.start.y - line.start.x*line.direction.y;
+		double c2 = a2 * line.start.x - b1 * line.start.y;
 		
 		double denom = a1*b2 - a2*b1;
-		if (Math.abs(denom) < 0.001) {
+		if (Math.abs(denom) < 1e-5) {
 			//the two lines are parallel
 			return Vector2D.NAN_VEC;
 		}
 		
-		double x = (b1*c2 - b2*c1)/denom;
-		double y = (a2*c1 - a1*c2)/denom;
+		double x = (b2*c1 - b1*c2)/denom;
+		double y = (a1*c2 - a2*c1)/denom;
+		
+		return new Vector2D(x, y);*/
+		
+		//http://www-cs.ccny.cuny.edu/~wolberg/capstone/intersection/Intersection%20point%20of%20two%20lines.html
+		double x1 = start.x;
+		double x2 = start.x + direction.x;
+		double x3 = line.start.x;
+		double x4 = line.start.x + line.direction.x;
+
+		double y1 = start.y;
+		double y2 = start.y + direction.y;
+		double y3 = line.start.y;
+		double y4 = line.start.y + line.direction.y;
+		
+		double ua = ((x4-x3)*(y1-y3) - (y4-y3)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1));
+		
+		double x = x1 + ua * (x2-x1);
+		double y = y1 + ua * (y2-y1);
 		
 		return new Vector2D(x, y);
 	}
@@ -51,7 +69,7 @@ public class Line2D {
 	 * 		The minimum distance between the point and this line.
 	 */
 	public double calculateMinDistToPoint(Vector2D point) {
-        try {
+        /*try {
             //find the orthogonal vector on line
         	double[] normVec = new double[] {-direction.y * 100.0 / direction.x, 100.0};//define the second part as 100
         	//calculate the parameters
@@ -63,7 +81,15 @@ public class Line2D {
         }
         catch (Exception e) {
             return Double.NaN;
-        }
+        }*/
+		
+		//https://www.topcoder.com/community/data-science/data-science-tutorials/geometry-concepts-basic-concepts/
+		
+		//project the vector from a point of the line on the line; then calculate the distance between the two points
+		Vector2D toPoint = start.vectorTo(point);
+		Vector2D projected = direction.project(toPoint);
+		Vector2D p2 = start.add(projected);
+		return point.distance(p2);
     }
 	
 	/**
@@ -77,9 +103,15 @@ public class Line2D {
 	 */
 	public boolean isPointOnLine(Vector2D point) {
 		//the calculation of the parameter t is equal in all cases (x and y).
-		double t1 = direction.x == 0 ? 0 : ((start.x - point.x) / direction.x);
-		double t2 = direction.y == 0 ? 0 : ((start.y - point.y) / direction.y);
-		return Math.abs(t1-t2) < 1e-5 && t1 != 0;
+		double t1 = direction.x == 0 ? 0 : -((start.x - point.x) / direction.x);
+		double t2 = direction.y == 0 ? 0 : -((start.y - point.y) / direction.y);
+		if (direction.x == 0) {
+			return Math.abs(start.x - point.x) < 1e-8;
+		}
+		else if (direction.y == 0) {
+			return Math.abs(start.y - point.y) < 1e-8;
+		}
+		return Math.abs(t1-t2) < 1e-5;
 	}
 	
 	/**
@@ -94,11 +126,12 @@ public class Line2D {
 	public double calculateT(Vector2D point) {
 		if (isPointOnLine(point)) {
 			if (direction.x != 0) {
-				return ((start.x - point.x) / direction.x);				
+				return -((start.x - point.x) / direction.x);				
 			}
 			else {
-				return ((start.y - point.y) / direction.y);
+				return -((start.y - point.y) / direction.y);
 			}
+			//if direction.x and direction.y is 0 it's no line...
 		}
 		return Double.NaN;
 	}
---
+++ test/net/jfabricationgames/linear_algebra/Line2DTest.java
@@ -0,0 +1,72 @@
+package net.jfabricationgames.linear_algebra;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Line2DTest {
+	
+	public static final double EPSILON = 1e-8;
+	
+	@Test
+	public void testCalculateCrosspoint() {
+		Line2D l1 = new Line2D(new Vector2D(0, 0), new Vector2D(1, 1));
+		Line2D l2 = new Line2D(new Vector2D(0, 0), new Vector2D(1, 2));
+		Line2D l3 = new Line2D(new Vector2D(2, 0), new Vector2D(-1, 1));
+		
+		assertEquals(new Vector2D(0, 0), l1.calculateCrosspoint(l2));
+		assertEquals(new Vector2D(0, 0), l2.calculateCrosspoint(l1));
+		assertEquals(new Vector2D(1, 1), l1.calculateCrosspoint(l3));
+		assertEquals(new Vector2D(1, 1), l3.calculateCrosspoint(l1));
+		assertEquals(new Vector2D(2.0/3.0, 4.0/3.0), l2.calculateCrosspoint(l3));
+	}
+	
+	@Test
+	public void testCalculateMinDistToPoint() {
+		Line2D l1 = new Line2D(new Vector2D(0, 0), new Vector2D(1, 1));
+		Line2D l2 = new Line2D(new Vector2D(0, 1), new Vector2D(1, 0));
+		
+		//point on line
+		assertEquals(0, l1.calculateMinDistToPoint(new Vector2D(0, 0)), EPSILON);
+		assertEquals(0, l1.calculateMinDistToPoint(new Vector2D(1, 1)), EPSILON);
+		
+		//line: y = 1
+		assertEquals(1, l2.calculateMinDistToPoint(new Vector2D(0, 2)), EPSILON);
+		assertEquals(4, l2.calculateMinDistToPoint(new Vector2D(3, 5)), EPSILON);
+		
+		//line: y = x
+		assertEquals(Math.sqrt(2), l1.calculateMinDistToPoint(new Vector2D(0, 2)), EPSILON);
+		assertEquals(Math.sqrt(2)*0.5, l1.calculateMinDistToPoint(new Vector2D(1, 0)), EPSILON);
+	}
+	
+	@Test
+	public void testIsPointOnLine() {
+		Line2D l1 = new Line2D(new Vector2D(0, 0), new Vector2D(1, 1));
+		Line2D l2 = new Line2D(new Vector2D(0, 1), new Vector2D(1, 0));
+		
+		assertTrue(l1.isPointOnLine(new Vector2D(0, 0)));
+		assertTrue(l1.isPointOnLine(new Vector2D(4, 4)));
+		assertTrue(l2.isPointOnLine(new Vector2D(0, 1)));
+		assertTrue(l2.isPointOnLine(new Vector2D(5, 1)));
+
+		assertFalse(l1.isPointOnLine(new Vector2D(1, 0)));
+		assertFalse(l1.isPointOnLine(new Vector2D(0, 1)));
+		assertFalse(l2.isPointOnLine(new Vector2D(0, 5)));
+		assertFalse(l2.isPointOnLine(new Vector2D(3, 4)));
+	}
+	
+	@Test
+	public void testCalculateT() {
+		Line2D l1 = new Line2D(new Vector2D(0, 0), new Vector2D(1, 1));
+		Line2D l2 = new Line2D(new Vector2D(0, 1), new Vector2D(1, 0));
+		
+		assertEquals(0, l1.calculateT(new Vector2D(0, 0)), EPSILON);
+		assertEquals(4, l1.calculateT(new Vector2D(4, 4)), EPSILON);
+		assertEquals(0, l2.calculateT(new Vector2D(0, 1)), EPSILON);
+		assertEquals(5, l2.calculateT(new Vector2D(5, 1)), EPSILON);
+
+		assertEquals(Double.NaN, l1.calculateT(new Vector2D(1, 0)), EPSILON);
+		assertEquals(Double.NaN, l2.calculateT(new Vector2D(0, 5)), EPSILON);
+	}
+	
+}
